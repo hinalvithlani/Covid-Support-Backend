2ec320da7ce01b699751d655b038af50ade10a42
vinayak dubey <vinayak.dubey2019@vitstudent.ac.in>
2021-05-09 14:22:19 +0530

    phone number removed from DB

--- app.js
+++ app.js
@@ -28,14 +28,13 @@ app.post("/Add", function(req, res){
         console.log("validation failed")
         return
     }
-    req.body.phone_number=parseInt(req.body.phone_number);
     req.body.beds=parseInt(req.body.beds);
     req.body.oxygen=parseInt(req.body.oxygen);
     req.body.provider_contact = parseInt(req.body.provider_contact)
     req.body.helpful = parseInt(req.body.helpful)
     req.body.not_helpful = parseInt(req.body.not_helpful)
-    var data = {name, email,phone_number, provider_contact, state, city, location, beds, oxygen, others, helpful, not_helpful, verified} = req.body
-    firebase.database().ref(req.body.state + "/"+req.body.city+"/"+req.body.location).set({name, email, phone_number, provider_contact, state, city, location, beds, oxygen, others, helpful, not_helpful, verified});
+    var data = {name, email, provider_contact, state, city, location, beds, oxygen, others, helpful, not_helpful, verified} = req.body
+    firebase.database().ref(req.body.state + "/"+req.body.city+"/"+req.body.location).set({name, email, provider_contact, state, city, location, beds, oxygen, others, helpful, not_helpful, verified});
     console.log(req.body)
     res.json({
         status: "successful"
@@ -77,7 +76,7 @@ function validate(data){
         return false
     else if(!data.email.match(emailRegex))
         return false
-    else if(isNaN(data.phone_number) || isNaN(data.beds) || isNaN(data.oxygen) || isNaN(data.provider_contact))
+    else if(isNaN(data.beds) || isNaN(data.oxygen) || isNaN(data.provider_contact))
         return false
     else if(!(typeof data.verified ==="boolean") || isNaN(data.helpful) || isNaN(data.not_helpful))
         return false
